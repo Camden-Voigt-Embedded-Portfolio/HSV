1291935365d11063c3c08158a7d893143484da8d
Camden Voigt <camdenvoigt@gmail.com>
2026-03-04 18:03:31 -0800

    Fixed Buttons

--- src/main.rs
+++ src/main.rs
@@ -53,30 +53,35 @@ fn update_hsv(hsv: Hsv, new_val: i16, mode: Mode) -> Hsv {
     }
 }
 
-struct AppState {
-    gpiote: Gpiote,
-    a_button: Pin<Input<Floating>>,
-    b_button: Pin<Input<Floating>>,
+struct Buttons {
+    timer_a: Timer<pac::TIMER2>,
+    timer_b: Timer<pac::TIMER3>,
+    button_a: Pin<Input<Floating>>,
+    button_b: Pin<Input<Floating>>,
 }
-
-static TIMER_MUT: LockMut<RgbDisplay> = LockMut::new();
-static APP_STATE: LockMut<AppState> = LockMut::new();
+static DEBOUNCE_BUTTONS: LockMut<Buttons> = LockMut::new();
+static GPIOTE: LockMut<Gpiote> = LockMut::new();
 static A_BUTTON_STATE: AtomicBool = AtomicBool::new(false);
 static B_BUTTON_STATE: AtomicBool = AtomicBool::new(false);
 
+static TIMER_MUT: LockMut<RgbDisplay> = LockMut::new();
+
+const DEBOUNCE_TIME: u32 = 100 * 1_000_000 / 1000;
 #[interrupt]
 fn GPIOTE() {
-    APP_STATE.with_lock(|app_state| {
-        let a_button_changed = app_state.gpiote.channel0().is_event_triggered();
-        if a_button_changed {
-            A_BUTTON_STATE.store(app_state.a_button.is_low().unwrap(), Release);
+    DEBOUNCE_BUTTONS.with_lock(|buttons| {
+        if buttons.timer_a.read() == 0 {
+            A_BUTTON_STATE.store(buttons.button_a.is_low().unwrap(), Release);
+            buttons.timer_a.start(DEBOUNCE_TIME);
         }
-        let b_button_changed = app_state.gpiote.channel1().is_event_triggered();
-        if b_button_changed {
-            B_BUTTON_STATE.store(app_state.b_button.is_low().unwrap(), Release);
+        if buttons.timer_b.read() == 0 {
+            B_BUTTON_STATE.store(buttons.button_b.is_low().unwrap(), Release);
+            buttons.timer_b.start(DEBOUNCE_TIME);
         }
-        app_state.gpiote.channel0().reset_events();
-        app_state.gpiote.channel1().reset_events();
+    });
+    GPIOTE.with_lock(|gpiote| {
+        gpiote.channel0().reset_events();
+        gpiote.channel1().reset_events();
     });
 }
 
@@ -94,6 +99,8 @@ fn main() -> ! {
     let mut display = Display::new(board.display_pins);
     let mut timer0 = Timer::new(board.TIMER0);
     let mut timer1 = Timer::new(board.TIMER1);
+    let timer2 = Timer::new(board.TIMER2);
+    let timer3 = Timer::new(board.TIMER3);
     let mut a_button = board.buttons.button_a.degrade();
     let mut b_button = board.buttons.button_b.degrade();
     let pin_r = board.edge.e08.into_push_pull_output(Level::High);
@@ -105,8 +112,7 @@ fn main() -> ! {
     let saadc_config = SaadcConfig::default();
     let mut saadc = Saadc::new(board.ADC, saadc_config);
 
-    // needed setup
-    let gpiote = Gpiote::new(board.GPIOTE);
+    // NVIC setup
     unsafe {
         pac::NVIC::unmask(pac::Interrupt::GPIOTE);
         pac::NVIC::unmask(pac::Interrupt::TIMER0);
@@ -114,30 +120,37 @@ fn main() -> ! {
     pac::NVIC::unpend(pac::Interrupt::GPIOTE);
     pac::NVIC::unpend(pac::Interrupt::TIMER0);
 
+    // Setup GPIOTE
+    let gpiote = Gpiote::new(board.GPIOTE);
     gpiote
         .channel0()
         .input_pin(&a_button)
-        .toggle()
+        .hi_to_lo()
         .enable_interrupt();
     gpiote
         .channel1()
         .input_pin(&b_button)
-        .toggle()
+        .hi_to_lo()
         .enable_interrupt();
-    timer0.enable_interrupt();
+    GPIOTE.init(gpiote);
 
+    // Setup buttons
     A_BUTTON_STATE.store(a_button.is_low().unwrap(), Release);
     B_BUTTON_STATE.store(b_button.is_low().unwrap(), Release);
-    let mut mode = Mode::Hue;
+    DEBOUNCE_BUTTONS.init(Buttons {
+        timer_a: timer2,
+        timer_b: timer3,
+        button_a: a_button,
+        button_b: b_button,
+    });
+
+    // Setup timer for PWM
+    timer0.enable_interrupt();
     let rgb_display = RgbDisplay::new(rgb_pins, timer0);
     TIMER_MUT.init(rgb_display);
-    let app_state = AppState {
-        gpiote,
-        a_button,
-        b_button,
-    };
-    APP_STATE.init(app_state);
 
+    // Setup State
+    let mut mode = Mode::Hue;
     let mut hsv = Hsv {
         h: 0.5,
         s: 1.0,
@@ -160,6 +173,8 @@ fn main() -> ! {
             (false, true, m) => m.get_next(),
             (false, false, m) => m,
         };
+        A_BUTTON_STATE.store(false, Release);
+        B_BUTTON_STATE.store(false, Release);
 
         // Handle HSV update and conversion
         let value = saadc.read_channel(&mut pin_2).unwrap();
